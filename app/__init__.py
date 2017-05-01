81194203855353dcd0e05f603e6cdd8e41a1c107
KRMorris <rssnmorris@yahoo.com>
2017-05-01 02:01:36 -0500

    connect

--- app/__init__.py
+++ app/__init__.py
@@ -11,11 +11,24 @@ from views import AddUser#, imgList
 from image_getter import ImageGetter
 from getTitle import TitleGet
 
+import psycopg2
+import urlparse
+
+urlparse.uses_netloc.append("postgres")
+url = urlparse.urlparse(os.environ["DATABASE_URL"])
+
+conn = psycopg2.connect(
+    database=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port
+)
 #dev
 #app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://info3180proj1:info3180@localhost:5433/info3180"
 
 #prod
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 
 app.add_url_rule('/api/user/register', view_func=AddUser.as_view('addUser'),
     methods=['POST'])
